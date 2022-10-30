4d19f03b15a88f63b7c146c6bab166c118063527
CalipsoAdara <lucia.castro.5c@gmail.com>
2022-10-30 12:35:48 -0300

    Agregar buscar fechas buenas del NRL

--- check_miembros.R
+++ check_miembros.R
@@ -75,14 +75,9 @@ outDir=paste0(inPath,varname,plevstr,'/daily/ensmean/',group,'-',model,'/');
 dir.create(outDir,recursive=TRUE)
 
 # Create vector of initialization dates per week (only the first of the 4)
-dateinits=seq(as.Date("1999/01/02"), as.Date("2015/12/26"), by="7 day")
+dateinits <- sttdate
 
-date1=list()
-date2=list()
-date3=list()
-date4=list()
-dates = list(date1,date2,date3,date4)
-a =array(NA, dim = c(length(dateinits),nenss))
+
 
 
 for(idate in 1:length(dateinits)){
@@ -290,3 +285,88 @@ for(imodel in 1:nmodels){
 saveRDS(li.ar,file="SubX_processed_Rdata/miembros/ar_miembros.rds")
 
 
+#------------------------------------------------------------------------------------------------------------
+# ENCONTRAR LAS FECHAS CORRESPONDIENTES DEL NRL-NESM
+
+dateinits=seq.Date(as.Date("1999-01-02"),as.Date("2014-12-31"),1)
+
+
+inPath='/datos/SubX/hindcast/'
+group=c('NRL')                        # Modeling Groups (must be same # elements as models below)
+model=c('NESM')               # Model Name (must be same # of elements as groups above)
+
+varname=c('tas')
+plevstr=c('sfc')
+# Define input directory
+inDir1=paste0(inPath,varname,plevstr,'/daily/anom/',group,'-',model,'/') # input filename for anomalies data
+sttdate = list()
+
+for (idate in 1:length(dateinits)) {
+  
+  print(paste("Check de la fecha:",dateinits[idate]))
+  
+  im=month(dateinits[idate])
+  mm=as.character(im)
+  if(im<10){mm=paste0('0',mm)}
+  idy=day(dateinits[idate])
+  dd=as.character(idy)
+  if(idy<10){dd=paste0('0',dd)}
+  yy=year(dateinits[idate])
+  yyyymmdd1=paste0(as.character(yy),mm,dd)
+  
+  im=month(dateinits[idate]+1)
+  mm=as.character(im)
+  if(im<10){mm=paste0('0',mm)}
+  idy=day(dateinits[idate]+1)
+  dd=as.character(idy)
+  if(idy<10){dd=paste0('0',dd)}
+  yy=year(dateinits[idate]+1)
+  yyyymmdd2=paste0(as.character(yy),mm,dd)
+  
+  im=month(dateinits[idate]+2)
+  mm=as.character(im)
+  if(im<10){mm=paste0('0',mm)}
+  idy=day(dateinits[idate]+2)
+  dd=as.character(idy)
+  if(idy<10){dd=paste0('0',dd)}
+  yy=year(dateinits[idate]+2)
+  yyyymmdd3=paste0(as.character(yy),mm,dd)
+  
+  im=month(dateinits[idate]+3)
+  mm=as.character(im)
+  if(im<10){mm=paste0('0',mm)}
+  idy=day(dateinits[idate]+3)
+  dd=as.character(idy)
+  if(idy<10){dd=paste0('0',dd)}
+  yy=year(dateinits[idate]+3)
+  yyyymmdd4=paste0(as.character(yy),mm,dd)
+  
+  # Loop over all "ensemble members"
+  
+  # Define files
+  inFname1=paste0(inDir1,varname,'_',as.character(plevstr),'_',group,'-',model,'_',yyyymmdd1,'.e1.anoms.daily.nc')
+  inFname2=paste0(inDir1,varname,'_',as.character(plevstr),'_',group,'-',model,'_',yyyymmdd2,'.e1.anoms.daily.nc')
+  inFname3=paste0(inDir1,varname,'_',as.character(plevstr),'_',group,'-',model,'_',yyyymmdd3,'.e1.anoms.daily.nc')
+  inFname4=paste0(inDir1,varname,'_',as.character(plevstr),'_',group,'-',model,'_',yyyymmdd4,'.e1.anoms.daily.nc')
+  
+  inFname=c(inFname1,inFname2,inFname3,inFname4)
+  bool = c()
+  for(e in 1:4) {
+    
+    # Read data
+    data = metR::ReadNetCDF(inFname[e], out='array')
+    bool[e] <- sum(is.na(data[[1]]))!=prod(size(data[[1]]))
+    
+  }
+  if (sum(bool)==4) {
+    # Guarda la primera fecha
+    sttdate[[idate]] <-as.Date(dateinits[idate])
+    
+  }
+  
+}
+
+print(sttdate)
+sttdate = Reduce(c, sttdate) # Convierte a vector manteniendo Dates
+
+saveRDS(sttdate,"./SubX_processed_Rdata/miembros/NRL_dates")
